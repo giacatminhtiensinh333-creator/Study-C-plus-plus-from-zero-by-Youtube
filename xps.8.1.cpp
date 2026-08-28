4dfbf1f9d6eb14a58940c85c33b88fa28ba6798e
Gia Cat Minh <giacatminhtiensinh333@gmail.com>
2026-08-28 10:16:33 +0700

    Add files via upload
    this is lesson today

---
+++ xps.8.1.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double r;
+	double cv;
+	double dt;
+	cout << "Moi nhap vao ban kinh: ";
+	cin >> r;
+	cv = 2 * r * M_PI;
+	dt = r * r * M_PI;
+	cout << "Chu vi hinh tron la: " << cv << endl << "Dien tich hinh tron la: " << dt << endl;
+
+	double a, b, CV, DT;
+	cout << "Moi nhap vao chieu dai: ";
+	cin >> a;
+	cout << "Moi nhap vao chieu rong: ";
+	cin >> b;
+	CV = (a + b) * 2;
+	DT = a * b;
+	cout << "Chu vi HCN la: " << CV << endl << "Dien tich HCn la: " << DT << endl;
+
+	double dtt, dv, da, dtb;
+	cout << "Moi nhap vao diem toan: ";
+	cin >> dtt;
+	cout << "Moi nhap vao diem van: ";
+	cin >> dv;
+	cout << "Moi nhap vao diem anh: ";
+	cin >> da;
+	dtb = (dtt + dv + da) / 3;
+	cout << "Diem trung binh cua ban la: " << setprecision(2) << dtb << endl;
+
+}
+
