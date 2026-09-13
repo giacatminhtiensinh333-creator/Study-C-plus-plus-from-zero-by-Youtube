6a03e690e44f37c3f8721d3cd0ebe7fa51a3922e
Gia Cat Minh <giacatminhtiensinh333@gmail.com>
2026-09-13 21:12:26 +0700

    Update xps.23.exercise.cpp

--- xps.23.exercise.cpp
+++ xps.23.exercise.cpp
@@ -3,6 +3,7 @@
 #include <random>
 using namespace std;
 
+//1.change variables' value
 void hoanvi(double *a, double *b) {
     double c = *a;
     *a = *b;
@@ -11,6 +12,7 @@ void hoanvi(double *a, double *b) {
 
 int main()
 {
+    //1.
     double a, b;
     cout << "Moi nhap vao so a: ";
     cin >> a;
@@ -19,13 +21,14 @@ int main()
     hoanvi(&a, &b);
     cout << "Hai so cua ban sau khi doi cho la: a= " << a <<", b= " << b << endl;
 
+    //2. create random array, print and descend it
     int array1way[10];
     int* arrayconsor;
     arrayconsor = array1way;
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dis(0, 100);
-    cout << "Mang truoc khi sap xep giam den la: \n";
+    cout << "Mang truoc khi sap xep giam dan la: \n";
     for (int i = 0; i < 10; i++)
     {
         *(arrayconsor+i) = dis(gen);
